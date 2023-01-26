6017b5ee2635adf122484bdc18d1c735c2243811
unknown <ebrahim195011@feng.bu.edu.eg>
2023-01-26 15:50:40 +0200

    Recursion & Backtracking.py has been modified

--- Recursion & Backtracking.py
+++ Recursion & Backtracking.py
@@ -1,3 +1,7 @@
+import sys
+sys.stdin = open("D:\Kaldesh\input.txt",'r')
+sys.stdout = open("D:\Kaldesh\output.txt",'w')
+
 # Backtracking
 
 # def func_name (para = initial):
@@ -11,3 +15,36 @@
 #      UNDO   
 #
 
+# Generate all permutations from 1 -> n
+def all_permutations(idx , a ,v, n):
+    if idx == n:
+        for i in range(n):
+            print(a[i],end=" ")
+        print()
+        return
+
+    for i in range(n):
+        if not v[i]:
+            a[idx] = i+1
+            v[i] = 1
+            all_permutations(idx+1 , a ,v, n)
+            v[i] = 0
+
+# Generate all Possible Solutions
+def all_possible_solution(idx , a , n):
+    if idx == n:
+        for i in range(n):
+            print(a[i],end=" ")
+        print()
+        return
+
+    for i in range(n):
+        a[idx] = i
+        all_possible_solution(idx+1 , a , n)
+
+a = [0 for i in range(5)]
+n = len(a)
+v = [0 for i in range(n+1)]
+
+all_permutations(0 , a , v , n)
+all_possible_solution(0 , a , n)
