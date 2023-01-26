219f96bdbf77299acca7e4ae49aab1f95dc2c7a6
unknown <ebrahim195011@feng.bu.edu.eg>
2023-01-26 15:30:45 +0200

    New file (Recursion & Backtracking) has been added

---
+++ Recursion & Backtracking.py
@@ -0,0 +1,13 @@
+# Backtracking
+
+# def func_name (para = initial):
+#    if (stop condition):
+#         Action
+#         Return 
+#     
+#     from i to j:
+#      DO
+#      func_name(para+1)
+#      UNDO   
+#
+
